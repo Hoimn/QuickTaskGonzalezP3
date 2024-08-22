e4c4123a78f01c0499c5b964018a8a929929bd50
Hoimn <737753@student.dusd.net>
2024-08-22 11:46:23 -0700

    Creaated a start menu
    Creaated a start menu

--- P3GonzalezQuickTask/Assets/Scripts/StartMenu.cs
+++ P3GonzalezQuickTask/Assets/Scripts/StartMenu.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
-   
-
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
