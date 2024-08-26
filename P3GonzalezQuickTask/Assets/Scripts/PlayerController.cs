4507c4f28d83a5579bd75b2f055978f2e9d269a2
Hoimn <737753@student.dusd.net>
2024-08-26 11:17:24 -0700

    Created the start menu
    Created the start menu

--- P3GonzalezQuickTask/Assets/Scripts/PlayerController.cs
+++ P3GonzalezQuickTask/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   
+    public float speed = 10;
+    public float jumpForce = 5;
+
+    private bool isOnGround;
+
+    SpriteRenderer playerSprite;
+    Rigidbody2D playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerSprite = GetComponent<SpriteRenderer>();
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerMove();
 
+    }
+    void PlayerMove()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+
+    transform.Translate(Vector2.right * horizontal * speed * Time.deltaTime);
+
+    if (horizontal >= 0)
+        {
+            playerSprite.flipX = false;
+        }
+    else
+        {
+            playerSprite.flipX = true;
+        }
+
+    if (Input.GetKeyDown(KeyCode.Space)&& isOnGround==true)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
     }
 }
